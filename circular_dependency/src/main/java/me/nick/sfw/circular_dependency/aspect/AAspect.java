dbad874a776efbff4aa5b214aa1b1966d8411466
Nick Ni <shuhong.ni@gientech.com>
2025-07-29 17:11:56 +0800

    aspect aop

--- circular_dependency/src/main/java/me/nick/sfw/circular_dependency/aspect/AAspect.java
+++ circular_dependency/src/main/java/me/nick/sfw/circular_dependency/aspect/AAspect.java
@@ -1,5 +1,14 @@
 package me.nick.sfw.circular_dependency.aspect;
 
+import org.aopalliance.intercept.Joinpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class AAspect {
 
+    @Before("execution(public void me.nick.sfw.circular_dependency.service.AService.test())")
+    public void aBefore(Joinpoint joinpoint) {
+        System.out.println("A Aspect");
+    }
 }
